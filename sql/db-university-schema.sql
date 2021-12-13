5cb2dd40116b552fe9a9e83ec60f0afc6864a1b5
ABozzoli <alessandrobozzoli92@gmail.com>
2021-12-13 17:31:27 +0100

    Agg. tab. exams, student, vote

--- sql/db-university-schema.sql
+++ sql/db-university-schema.sql
@@ -49,3 +49,39 @@ CREATE TABLE `teachers`(
 	`office_address` VARCHAR(100),
 	PRIMARY KEY(`id`)
 );
+
+-- TABELLA EXAMS
+CREATE TABLE `exams`(
+	`id`INT NOT NULL AUTO_INCREMENT,
+	`date` DATE NOT NULL,
+	`hour` TIME NOT NULL,
+	`location` VARCHAR(100),
+	`office_address` VARCHAR(100),
+	`courses_id` INT NOT NULL, -- FK
+	PRIMARY KEY(`id`),
+	FOREIGN KEY(`courses_id`) REFERENCES courses(`id`)
+);
+
+-- TABELLA STUDENT
+CREATE TABLE `student`(
+	`id`INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(100) NOT NULL,
+	`surname` VARCHAR(100) NOT NULL,
+	`date_of_birth` DATE,
+	`fiscal_code` VARCHAR(100),
+	`enrolment_date` DATE,
+	`registration_number` INT,
+	`email` VARCHAR(100),
+	`degrees_id` INT NOT NULL, -- FK
+	PRIMARY KEY(`id`),
+	FOREIGN KEY(`degrees_id`) REFERENCES degrees(`id`)
+);
+
+-- TABELLA *PONTE* VOTE
+CREATE TABLE `vote`(
+	`student_id` INT NOT NULL,
+	`exams_id` INT NOT NULL,
+	PRIMARY KEY(`sudent_id`, `exams_id`),
+	FOREIGN KEY(`student_id`) REFERENCES student(`id`),
+	FOREIGN KEY(`exams_id`) REFERENCES exams(`id`)
+);
