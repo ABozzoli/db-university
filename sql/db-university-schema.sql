e0ae475c6bf346707a55205c288f7857fbecab03
ABozzoli <alessandrobozzoli92@gmail.com>
2021-12-13 17:15:57 +0100

    Correz. png, agg. tab. courses e tab. teachers

--- sql/db-university-schema.sql
+++ sql/db-university-schema.sql
@@ -1,26 +1,51 @@
--- CREAZIONE
+-- CREAZIONE DATABASE
 CREATE DATABASE `test_university`;
 USE `test_university`;
 
+-- TABELLA DEPARTMENTS
 CREATE TABLE `departments`(
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(100) NOT NULL,
-	`address` VARCHAR(255),
-	`phone` TINYINT,
+	`address` VARCHAR(100),
+	`phone` INT,
 	`email` VARCHAR(100),
 	`website` VARCHAR(100),
 	`head_of_departments` VARCHAR(100),
 	PRIMARY KEY(`id`)
 );
 
+-- TABELLA DEGREES
 CREATE TABLE `degrees`(
 	`id`INT NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(255) NOT NULL,
+	`name` VARCHAR(100) NOT NULL,
 	`address` VARCHAR(100),
 	`email` VARCHAR(100),
 	`website` VARCHAR(100),
 	`level` VARCHAR(100),
-	`departments_id` INT NOT NULL,
+	`departments_id` INT NOT NULL, -- FK
 	PRIMARY KEY(`id`),
 	FOREIGN KEY(`departments_id`) REFERENCES departments(`id`)
+);
+
+-- TABELLA COURSES
+CREATE TABLE `courses`(
+	`id`INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(100) NOT NULL,
+	`description` VARCHAR(255),
+	`website` VARCHAR(100),
+	`cfu` INT,
+	`degrees_id` INT NOT NULL, -- FK
+	PRIMARY KEY(`id`),
+	FOREIGN KEY(`degrees_id`) REFERENCES degrees(`id`)
+);
+
+-- TABELLA TEACHERS
+CREATE TABLE `teachers`(
+	`id`INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(100) NOT NULL,
+	`surname` VARCHAR(100) NOT NULL,
+	`phone` INT,
+	`email` VARCHAR(100),
+	`office_address` VARCHAR(100),
+	PRIMARY KEY(`id`)
 );
