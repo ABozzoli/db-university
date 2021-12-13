db1d99cb94e146ca6aa47a6a67ab3c8ec595689c
ABozzoli <alessandrobozzoli92@gmail.com>
2021-12-13 18:05:15 +0100

    Ins. dati per courses, teacher, exams

--- sql/db-university-data.sql
+++ sql/db-university-data.sql
@@ -7,3 +7,13 @@ INSERT INTO `degrees`(`name`, `email`, `level`, `departments_id`) VALUES ('Laure
 INSERT INTO `degrees`(`name`, `email`, `level`, `departments_id`) VALUES ('Laurea in Ingegneria dell automazione', 'informatica@polimi.it', 'Triennale', 1);
 INSERT INTO `degrees`(`name`, `email`, `level`, `departments_id`) VALUES ('Laurea in Economia', 'economia@polimi.it', 'Triennale', 2);
 INSERT INTO `degrees`(`name`, `email`, `level`, `departments_id`) VALUES ('Laurea in Architettura urbanistica', 'architetturaurbanistica@polimi.it', 'Triennale', 3);
+
+INSERT INTO `courses`(`name`, `cfu`, `degrees_id`) VALUES ('Analisi matematica 2', 10, 1);
+INSERT INTO `courses`(`name`, `cfu`, `degrees_id`) VALUES ('Geometria e algebra lineare', 8, 1);
+
+INSERT INTO `teacher`(`name`, `surname`, `phone`) VALUES ('Guglielmo', 'Filippi', 3492766812);
+INSERT INTO `teacher`(`name`, `surname`, `phone`) VALUES ('Anastasia', 'Ventura', 3376912845);
+
+INSERT INTO `exams`(`date`, `hour`, `location`, `courses_id`) VALUES (2021-02-08, 10:00:00.0000000, 'Aula 27', 1);
+INSERT INTO `exams`(`date`, `hour`, `location`, `courses_id`) VALUES (2021-02-16, 12:00:00.0000000, 'Aula 5', 2);
+INSERT INTO `exams`(`date`, `hour`, `location`, `courses_id`) VALUES (2021-03-02, 9:30:00.0000000, 'Aula 12', 1);
